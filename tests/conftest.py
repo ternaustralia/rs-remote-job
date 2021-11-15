5cb8b94b58b1a741cafa27b6c669c5791eef1586
Gerhard Weis <g.weis@uq.edu.au>
2021-11-15 13:43:21 +1000

    add ssh_server fixture (using paramiko) for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+from pathlib import Path
 import pytest
 from flask_tern.testing.fixtures import basic_auth as base_auth  # noqa
 from flask_tern.testing.fixtures import cache_spec, monkeypatch_session  # noqa
 from ssh_cert_service import create_app
 
+import mock_ssh_server
+
+@pytest.fixture
+def cmds_config():
+    """Return path to cmds.json config file."""
+    return str(Path(__file__).with_name("cmds.json"))
 
 @pytest.fixture
 def app():
@@ -19,11 +26,28 @@ def app():
 @pytest.fixture
 def basic_auth(base_auth):  # noqa
     for key, value in base_auth.items():
-        base_auth[key]["coesra_uname"] = value["name"]
+        base_auth[key].claims["coesra_uname"] = value["name"]
     return base_auth
 
 
 @pytest.fixture
 def client(app, basic_auth):  # noqa
     return app.test_client()
-    
+
+
+@pytest.fixture
+def ssh_server():
+    with mock_ssh_server.Server() as server:
+        yield server
+
+@pytest.fixture
+def mock_ssh_cert_service(monkeypatch):
+
+    def get_keys():
+        return {
+            "private_key": Path(__file__).with_name("test_user.key").read_text(),
+            "public_key": Path(__file__).with_name("test_user.key.pub").read_text(),
+            "cert_key": Path(__file__).with_name("test_user.key-cert.pub").read_text(),
+        }
+
+    monkeypatch.setattr("resource_server.utils.common.get_keys", get_keys)
