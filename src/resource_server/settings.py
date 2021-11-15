6913fad69162e58d2373e53b3a16bf5cce447072
Gerhard Weis <g.weis@uq.edu.au>
2021-11-15 13:42:34 +1000

    cleanup setting to prepare for using flask_tern settings parser

--- src/resource_server/settings.py
+++ src/resource_server/settings.py
@@ -2,17 +2,3 @@
 #
 # This file is read before all other configuration sources
 #
-
-import os
-from pathlib import Path
-# This is the base url for the signing api 
-BASE_URL = os.environ["KEY_SIGNING_BASE_URL_ENV"] + "/api/v1.0"
-
-# SLURM master node
-MASTER_NODE_HOST = os.environ["MASTER_NODE_HOST_ENV"]
-MASTER_NODE_USER = os.environ["MASTER_NODE_USER_ENV"]
-MASTER_NODE_PORT = os.environ["MASTER_NODE_PORT_ENV"]
-
-COMMANDS_JSON_FILE = str(Path.cwd() / "config/config.json") 
-COMMANDS_JSON_SCHEMA = str(Path.cwd() / "config/config.schema.json")
-
